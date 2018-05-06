64aed0ee7e818fb2049e762bb757849a9d216d9c
Vterebenin <niceserver11@mail.ru>
2018-05-06 14:06:21 +0300

    fixes

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,7 @@
 class Pic < ApplicationRecord
 	acts_as_votable 
 	belongs_to :user
-	has_attached_file :image, styles: { :medium => "300x300>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { :medium => "300x300>" }, default_url: "/images/:style/missing.png", s3_region: ENV["AWS_REGION"]
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
